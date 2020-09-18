08ce18605a5a492f97efa9e10bede4adaf3a0f32
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-18 22:01:16 +0500

    updated files added

---
+++ app.js
@@ -0,0 +1,6 @@
+const signup = () => {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password")
+    console.log(email.value);
+    console.log(password.value)
+}
