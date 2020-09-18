3210e9c55b598a31ad35ac75284d449f4c2487d0
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-18 22:19:53 +0500

    coplete authoriztion wit email and password

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const signup = () => {
     let email = document.getElementById("email");
     let password = document.getElementById("password")
-    console.log(email.value);
-    console.log(password.value)
+
+
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+    
+    .then((result)=>{
+        console.log(result)
+    })
+    
+    .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error)
+      });
 }
+
