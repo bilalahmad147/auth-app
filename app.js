86726bc7f2b1cd649c8c390c3f875caeed1d66fd
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-19 16:23:47 +0500

    auth using FB added

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ const signup = () => {
 
 
     firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-    
-    .then((result)=>{
-        console.log(result)
-    })
-    
-    .catch(function(error) {
+
+        .then(() => {
+            console.log("User Successfully Registred")
+        })
+
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("User not registered")
+        });
+}
+
+const loginUsingFb = () => {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(error)
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(error)
-      });
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 }
-
