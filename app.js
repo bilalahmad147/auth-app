578924d229a1dcb07aa7dc27acea555abd6fb94d
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-09-20 08:42:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,9 @@ const signup = () => {
 const loginUsingFb = () => {
     var provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
         var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
         console.log(user.email)
-        // ...
     }).catch(function (error) {
         console.log(error.message)
     });
