aab3d5ecc972e0a87693dcdf85c75be745ae4a51
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-07 04:48:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,51 @@ import { storage } from './Firebase/firebase'
 function App() {
 
   const allInputs = { imgUrl: '' }
-  const [imgAsFile, setImgAsFile] = useState('')
-  const [imgAsUrl, setImgAsUrl] = useState(allInputs)
+  const [imageAsFile, setImageAsFile] = useState('')
+  const [imageAsUrl, setImageAsUrl] = useState(allInputs)
 
-  console.log(imgAsFile)
-  const handleImgAsFile = (e) => {
-    const image = e.target.files[0]
-    setImgAsFile(imageFile => (image))
+  console.log(imageAsFile)
+  const handleImgFile = (e) => {
+    if (e.target.file[0]) {
+      setImageAsFile(e.target.file[0] )
+    }
+  }
+
+  const handleFireBaseUpload = e => {
+    e.preventDefault()
+    console.log('start of upload')
+    // async magic goes here...
+    if (imageAsFile === '') {
+      console.error(`not an image, the image file is a ${typeof (imageAsFile)}`)
+    }
+    const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
+    //initiates the firebase side uploading 
+    uploadTask.on('state_changed',
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot)
+      }, (err) => {
+        //catches the errors
+        console.log(err)
+      }, () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        storage.ref('images').child(imageAsFile.name).getDownloadURL()
+          .then(fireBaseUrl => {
+            setImageAsUrl(prevObject => ({ ...prevObject, imgUrl: fireBaseUrl }))
+          })
+      })
   }
 
   return (
+
     <div className="App">
       <h1>blank and ready for image upload.</h1>
-      <form>
-        <input type="file" />
+      <form onSubmit={handleFireBaseUpload}>
+        <input onChange={() => { handleImgFile() }} type="file" /><br />
+        <button>upload to firebase</button>
       </form>
+      <img src={imageAsUrl.imgUrl} alt="image tag" />
     </div>
   );
 }
